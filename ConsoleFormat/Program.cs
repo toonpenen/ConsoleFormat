010926985e3a4b3b071dbe3b2c5d8fd19e8d84ad
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-10 10:13:04 +0100

    Add project files.

---
+++ ConsoleFormat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id1 = 1234567;
+            int id2 = 67890;
+            int id3 = 101112;
+
+            string name1 = "Jan";
+            string name2 = "Toon";
+            string name3 = "Kenan Kurda";
+
+            double d1 = 3.91;
+            double d2 = 6.15;
+            double d3 = 4.70;
+
+            Console.WriteLine();
+
+        }
+    }
+}
